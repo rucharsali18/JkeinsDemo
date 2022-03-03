70be075e466fe62cb34e5b7797db4a61bc183fc2
rucharsali18 <73346375+rucharsali18@users.noreply.github.com>
2022-03-03 16:53:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
    public static void main(String args[])
     {
       System.out.println("HI     Jekins..............."); 
-      System.out.println("changes");
+      System.out.println("Hello");
       System.out.println("Plugins.........");
    }
 }
